a18d276a6481c196016e108566a53ce31891add1
Pedro Silva <pe.j.silva@gmail.com>
2017-03-02 15:12:31 +0000

    - jwt auth and annotations install

--- config/app.php
+++ config/app.php
@@ -177,6 +177,8 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
+        'Tymon\JWTAuth\Providers\JWTAuthServiceProvider',
+        App\Providers\AnnotationsServiceProvider::class,
     ],
 
     /*
@@ -226,6 +228,9 @@ return [
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
 
+        'JWTAuth' => 'Tymon\JWTAuth\Facades\JWTAuth',
+        'JWTFactory' => 'Tymon\JWTAuth\Facades\JWTFactory',
+        'Uuid' => Webpatser\Uuid\Uuid::class,
     ],
 
 ];
